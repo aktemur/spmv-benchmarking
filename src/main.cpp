36c93106665e9de5c0b593b18f97a555f8b9d1d5
Erdem Sarili <erdem.sarili@sap.com>
2018-03-15 23:31:56 +0100

    Measure execution times

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,37 @@
 #include "parse_options.h"
 #include "mmmatrix.hpp"
 
+
+long long int deltaMicroseconds(
+    std::chrono::steady_clock::time_point &t2,
+    std::chrono::steady_clock::time_point &t1) {
+  return std::chrono::duration_cast<std::chrono::microseconds>(t2 -t1).count();
+}
+
 std::unique_ptr<MATRIX> loadMatrix(const std::string filename);
 
 int main(int argc, const char *argv[]) {
 
   auto cliOptions = parseCliOptions(argc, argv);
-
-  auto matrix = loadMatrix(cliOptions->mtxFile);
-
-
+  
   SpmvMethodRegistry& registry = SpmvMethodRegistry::instance();
   auto method = registry.getMethod(cliOptions->method);
 
+  auto readStart = std::chrono::high_resolution_clock::now();
+  auto matrix = loadMatrix(cliOptions->mtxFile);
+
+  auto initStart = std::chrono::high_resolution_clock::now();
   method->init(matrix);
+
+  auto computeStart = std::chrono::high_resolution_clock::now();
   method->compute(nullptr, nullptr,0);
+  auto computeEnd = std::chrono::high_resolution_clock::now();
+
+
+  std::cout << "Reading Matrix       : " << deltaMicroseconds(initStart, readStart) << "us" << std::endl <<
+               "Initializing Matrix  : " << deltaMicroseconds(computeStart, initStart) << "us" << std::endl <<
+               "Computing            : " << deltaMicroseconds(computeEnd, computeStart) << "us" << std::endl ;
+
 
   return 0;
 }
